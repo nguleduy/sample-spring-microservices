2b0de003eaf2f16523e49794a0e38cab41e9413a
Joseph <nguld@itsj-group.com>
2020-12-16 13:12:15 +0700

    clean up

--- customer-service/src/main/java/com/example/customerservice/controller/CustomerController.java
+++ customer-service/src/main/java/com/example/customerservice/controller/CustomerController.java
@@ -4,7 +4,6 @@ import com.example.customerservice.communication.AccountClient;
 import com.example.customerservice.model.Account;
 import com.example.customerservice.model.Customer;
 import com.example.customerservice.model.CustomerType;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,9 +13,6 @@ import java.util.List;
 import java.util.logging.Logger;
 
 public class CustomerController {
-
-  @Autowired
-  RabbitTemplate template;
 
   @Autowired
   private AccountClient accountClient;
@@ -54,10 +50,4 @@ public class CustomerController {
     return customer;
   }
 
-  @RequestMapping("/customers/add/{id}/{identificationCard}")
-  public Customer addCustomer(@PathVariable("id") Integer id, @PathVariable("identificationCard") String identificationCard) {
-    logger.info(String.format("Customer.add(%d, %s)", id, identificationCard));
-    template.convertAndSend(id + "-" + identificationCard);
-    return new Customer(id, identificationCard, "Test Test", CustomerType.INDIVIDUAL);
-  }
 }
